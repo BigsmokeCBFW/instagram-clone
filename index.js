ee23858f08a32200c4a0fc978be6e9bd399da984
Ahmed Omeiza <omeizaahmed81@gmail.com>
2025-08-03 14:21:14 +0100

    rendering through js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: 212003
     },
     {
         name: "Gustave Courbet",
@@ -15,7 +15,7 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 41000
     },
         {
         name: "Joseph Ducreux",
@@ -24,7 +24,39 @@ const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152357
     }
 ]
 
+const mainEl = document.getElementById('main')
+
+function renderPost() { 
+    mainEl.innerHTML = posts.map((postObj) =>{
+        const {name, username, location, avatar, post, comment, likes} = postObj
+        return`
+            <div class="post-container">
+                <div class="user-info">
+                    <img src="${avatar}" alt="${name}'s profile picture " class="avatar">
+                    <span>
+                        <h2 class="user-name">${username}</h2>
+                        <h3 class="location-text">${location}</h3>
+                    </span>
+                </div>
+
+                <img src="${post}" alt="selfie artwork from ${name}" class="post-img">
+
+                <div class="post-content">
+                    <div class="react-btn">
+                        <img src="images/icon-heart.png" alt="like icon" class="icon">
+                        <img src="images/icon-comment.png" alt="comment icon" class="icon">
+                        <img src="images/icon-dm.png" alt="share icon" class="icon">
+                    </div>
+                    <h4>${likes.toLocaleString('en-US')}</h4>
+                    <h5><span class="bold">${username}</span> ${comment}</h5>
+                </div>
+            </div>
+    ` }).join("")
+
+}
+
+renderPost()
